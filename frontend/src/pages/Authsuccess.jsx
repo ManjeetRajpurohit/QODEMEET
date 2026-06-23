65c70c393678f87dc4fb52a9c197f4332aaed27e
ManjeetRajpurohit <manjeetrajpurohit81@gmail.com>
2026-06-23 15:42:30 +0530

    Update Authsuccess.jsx

--- frontend/src/pages/Authsuccess.jsx
+++ frontend/src/pages/Authsuccess.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { AppContext } from "../context/Appcontext";
 
 const Authsuccess = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
+  const { setToken } = useContext(AppContext);
 
   useEffect(() => {
     const token = searchParams.get("token");
 
     if (token) {
       localStorage.setItem("token", token);
+      setToken(token);
       navigate("/dashboard");
     } else {
       navigate("/login");
