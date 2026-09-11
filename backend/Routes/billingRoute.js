dc82138434654afab3bc8084abf419541dde9b87
ManjeetRajpurohit <manjeetrajpurohit81@gmail.com>
2026-09-11 23:58:28 +0530

    Update billingRoute.js

--- backend/Routes/billingRoute.js
+++ backend/Routes/billingRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {AddBill,getSingleBill,listAllBills, razorpayBill,verifyBill} from '../controllers/billingController.js';
 import userAuth from "../middleware/userAuth.js";
+import requireVerified from "../middleware/requireVerified.js";
 const billingRouter=express.Router();
 
 billingRouter.get('/',listAllBills);
 billingRouter.get('/:id',getSingleBill);
 billingRouter.post('/add',AddBill);
-billingRouter.post('/razorpay',userAuth,razorpayBill);
+billingRouter.post('/razorpay',userAuth,requireVerified,razorpayBill);
 billingRouter.post("/verify", userAuth, verifyBill);
 export default billingRouter;
