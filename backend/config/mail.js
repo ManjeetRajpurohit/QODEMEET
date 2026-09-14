823d09591848b6323fa7ab885a4688781fe5c027
ManjeetRajpurohit <manjeetrajpurohit81@gmail.com>
2026-09-14 13:56:10 +0530

    Update mail.js

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,7 +1,12 @@
+import dns from "node:dns";
 import nodemailer from "nodemailer";
 
+dns.setDefaultResultOrder("ipv4first");
+
 const gmailTransport = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_FROM,
     pass: process.env.EMAIL_APP_PASSWORD,
