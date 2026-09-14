27b997bb2804cb3989706c768c9c7aaf1da829e6
ManjeetRajpurohit <manjeetrajpurohit81@gmail.com>
2026-09-14 14:09:50 +0530

    Update mail.js

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,1 +1,27 @@
+import nodemailer from "nodemailer";
 
+const gmailTransport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  family: 4,
+  connectionTimeout: 15000,
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_APP_PASSWORD,
+  },
+});
+
+const transporter = {
+  sendMail: async ({ to, subject, html }) => {
+    return gmailTransport.sendMail({
+      from: `"QodeMeet" <${process.env.EMAIL_FROM}>`,
+      to,
+      subject,
+      html,
+    });
+  },
+};
+
+export default transporter;
